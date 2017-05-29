599bf7dac651f378d1ef8ab31b32a53a1739fe1f
Stephane Larocque <stephl001@gmail.com>
2017-05-29 14:48:52 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var _ = require('lodash');
+
+var app = express();
+
+var sequences = {};
+
+app.get('/sequence/:id', function (req, res) {
+  var id = _.toLower(req.params.id);
+  if (!_.has(sequences, _.toLower(id) )) {
+      sequences[id] = 0;
+  }
+  var currentValue = sequences[id];
+  sequences[id] += 1;
+  res.send({value: currentValue});
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
