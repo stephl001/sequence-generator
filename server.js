41f4e7176c3b130d8135889e3a98ec35d08e0035
Stephane Larocque <stephl001@gmail.com>
2017-06-02 09:25:17 -0400

    Add support for resetting sequence entries

--- server.js
+++ server.js
@@ -8,11 +8,23 @@ var sequences = {};
 app.get('/sequence/:id', function (req, res) {
   var id = _.toLower(req.params.id);
   if (!_.has(sequences, _.toLower(id) )) {
+      console.log(`Creating new sequence entry for id [${id}]`);
       sequences[id] = 0;
   }
   var currentValue = sequences[id];
   sequences[id] += 1;
   res.send({value: currentValue});
+});
+
+app.delete('/sequence/:id', function (req, res) {
+  var id = _.toLower(req.params.id);
+  if (_.has(sequences, _.toLower(id) )) {
+      console.log(`Removing sequence entry for id [${id}]`);
+      delete sequences[_.toLower(id)];
+      res.sendStatus(200);
+  } else {
+      res.sendStatus(404);
+  }
 });
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
